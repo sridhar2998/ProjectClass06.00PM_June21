84fecf790f33e48810abdc8f7591c6d718dc84fd
sridhar2998 <kingsridhar2901@gmail.com>
2021-07-02 16:47:37 +0530

    sri updated code

--- src/test/java/org/sample/Sample1.java
+++ src/test/java/org/sample/Sample1.java
@@ -3,7 +3,7 @@ package org.sample;
 public class Sample1 {
 	
 	public static void main(String[] args) {
-		System.out.println("Done===");
+		System.out.println("Done===");	
+		System.out.println("Test Success ");
 	}
-
 }
